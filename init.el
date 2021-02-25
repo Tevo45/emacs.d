841e313df19d18ff4e5f9aad0d1753d8c24c66d5
Estevan Castilho (Tevo) <estevan.cps@gmail.com>
2021-02-25 21:33:32 +0000

    Bikeshedding

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 
-;; packages 'n stuff
+;;; packages 'n stuff
 (require 'package)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
@@ -9,7 +9,7 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
-;;; use-package
+;; use-package
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 
@@ -18,53 +18,54 @@
 
 (setq use-package-always-ensure t)
 
-;;; auto-updates
+;; auto-updates
 (use-package auto-package-update
   :config
   (setq auto-package-update-delete-old-versions t)
   (setq auto-package-update-hide-results t)
   (auto-package-update-maybe))
 
-;;; shell environment
+;; shell environment
 (use-package exec-path-from-shell
   :config
   (when (or (memq window-system '(mac ns x)) (daemonp))
     (exec-path-from-shell-initialize)))
 
-;;; so-long
+;; so-long
 (unless (require 'so-long nil :noerror)
   (use-package so-long))
 (require 'so-long)
 (global-so-long-mode 1)
 
+;; magit
 (use-package magit)
 
-;; general options
-;;; for standalone elisp files
-(add-to-list 'load-path (concat user-emacs-directory "lisp/"))
+;;; general options
+;; for standalone elisp files
+(add-to-list 'load-path (concat user-emacs-directory "elisp/"))
 
-;;; line numbers and fill column
+;; line numbers and fill column
 (add-hook 'prog-mode-hook
 	  (lambda ()
 	    (display-line-numbers-mode)
 	    (display-fill-column-indicator-mode)))
 
-;;; so custom won't dump it's stuff here
+;; so custom won't dump it's stuff here
 (let ((custom (concat user-emacs-directory "custom.el")))
   (setq custom-file custom)
   (when (file-exists-p custom)
     (load-file custom)))
 
-;;; damn emacs, stop littering my filesystem already
+;; damn emacs, stop littering my filesystem already
 (setq backup-directory-alist
       `((".*" . ,(concat user-emacs-directory "/backup"))))
 (setq auto-save-file-name-transforms
       `((".*" ,(concat user-emacs-directory "/autosave"))))
 
-;;; acme-style mouse chording >:D
+;; acme-style mouse chording >:D
 (require 'acme-mouse)
 
-;; fancyness
+;;; fancyness
 (use-package brutal-theme
   :config
   (enable-theme 'brutal))
@@ -74,20 +75,20 @@
   (setq dashboard-startup-banner 'logo)
   (dashboard-setup-startup-hook))
 
-;; os-specific configuration
+;;; os-specific configuration
 (let ((conf (concat user-emacs-directory "/os/"
 		    (symbol-name system-type) ".el")))
   (when (file-exists-p conf)
     (load-file conf)))
 
-;; language support files
+;;; language support files
 (mapc #'load-file
       (directory-files (concat user-emacs-directory "/languages")
 		       t
 		       ".el$"))
 
-;; machine-specific configuration
-(let ((conf (concat user-emacs-directory "/cfg/"
+;;; machine-specific configuration
+(let ((conf (concat user-emacs-directory "/host/"
 		    (downcase (system-name)) ".el")))
   (when (file-exists-p conf)
     (load-file conf)))
